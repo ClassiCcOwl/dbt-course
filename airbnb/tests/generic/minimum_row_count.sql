3e54166e2fd2b6831b32943b1667d244b503d0aa
classiccowl <khavari.7878@yahoo.com>
2026-09-08 18:48:45 +0330

    Feat: custom generic tests for positive_values and minimum_row_count

---
+++ airbnb/tests/generic/minimum_row_count.sql
@@ -0,0 +1,12 @@
+{% test minimum_row_count(model, min_row_count) %}
+
+{{ config(severity='warn') }}
+SELECT
+    COUNT(*) AS cnt
+FROM
+     {{ model }}
+HAVING
+
+COUNT(*) < {{ min_row_count }}
+
+{% endtest %}
