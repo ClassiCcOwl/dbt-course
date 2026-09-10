fa90cfaac439c0a07de5cc5a167d9bc6e1832568
classiccowl <khavari.7878@yahoo.com>
2026-09-10 15:49:26 +0330

    Feat: enhance learn_log macro to include info level logging

--- airbnb/macros/logging.sql
+++ airbnb/macros/logging.sql
@@ -2,4 +2,8 @@
     {{ log(
         'Learn Log: This is a custom macro for logging messages in dbt.'
     ) }}
+    {{ log(
+        'Learn Log: This is a custom macro for logging info messages in dbt.',
+        info = True
+    ) }}
 {% endmacro %}
